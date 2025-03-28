b1f7017a6b6c6c1fbde52e7ef2dddb3ba3c65b9e
Nayan99 <nayanabildin@gmail.com>
2025-03-28 21:33:47 +0500

    добавил валидацию email, хеширование пароля, метод для получения заказов и отмены заказа

--- pages/user.py
+++ pages/user.py
@@ -1,14 +1,24 @@
 # класс для пользователя, который будет хранить информацию о пользователе и его заказах
+import hashlib
+
 from pages.order import Order
 
 
 class User:
     def __init__(self, name, email, password):
+        # валидация email
+        if "@" not in email or "." not in email:
+            raise ValueError("Некорректный email")
+
         self.name = name
         self.email = email
-        self.password = password
+        self.password = self.hash_password(password)
         # список заказов пользователя
         self.orders = []
+
+    # метод хеширования пароля
+    def hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
 
     # метод который возвращает строковые представление пользователя
     def __str__(self):
@@ -19,3 +29,15 @@ class User:
         order = Order(cart, self, payment_method, delivery_method)
         self.orders.append(order)
         return order
+
+    # метод получения всех заказов пользователя
+    def get_orders(self):
+        return self.orders
+
+    # метод для отмены заказа
+    def cancel_order(self, order):
+        if order in self.orders:
+            self.orders.remove(order)
+            return f"Заказ {order} был отменен"
+        else:
+            return "Заказ не найден"
